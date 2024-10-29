11a27b4993efd6d3ee5c14a3417077dc52c77a4c
Grace Hach <ghach@n0352.talapas.uoregon.edu>
2024-10-28 21:31:23 -0700

    I swear to god it's working

--- src/trim_primers.sh
+++ src/trim_primers.sh
@@ -3,8 +3,9 @@
 #SBATCH --partition=bgmp
 #SBATCH --account=bgmp
 #SBATCH --job-name=trim_primers
-#SBATCH --output=LOG/trim_primers_%j.err
-#SBATCH --error=LOG/trim_primers_%j.out          
+#SBATCH --output=LOG/trim_primers_%j.out
+#SBATCH --error=LOG/trim_primers_%j.err
+#SBATCH --time=10-00:00:00           
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=ghach@uoregon.edu
 
@@ -20,11 +21,13 @@ reverse_primers="../primers/CVR205stub_REV.fasta"
 # Read in names of merged files
 the_files=("${source_dir1}"*_MERGED.fastq)
 
+echo "I beseech you, in the bowels of Christ, think it possible you may be mistaken."
+
 for file in "${the_files[@]}"; do
     sliced_filename=${file##${source_dir1}}
-    echo "processing $destination_dir1${sliced_filename%%_MERGED*}"
+    echo "processing ${file}"
     hts_Primers -U ${file} -f "${destination_dir1}${sliced_filename%%_MERGED*}" \
-   -P ${forward_primers} -Q ${reverse_primers} -l 5 -x -e 6 -d 6 -L "hts_primers_output_blue" -F
+   -P ${forward_primers} -Q ${reverse_primers} -l 5 -x -e 6 -d 6 -L "hts_primers_output_BLUE" -F
 done
 
 echo "Blue complete."
@@ -33,7 +36,7 @@ the_files=("${source_dir2}"*_MERGED.fastq)
 
 for file in "${the_files[@]}"; do
     sliced_filename=${file##${source_dir2}}
-    echo "processing $destination_dir2${sliced_filename%%_MERGED*}"
+    echo "processing ${file}"
     hts_Primers -U ${file} -f "${destination_dir2}${sliced_filename%%_MERGED*}" \
-   -P ${forward_primers} -Q ${reverse_primers} -l 5 -x -e 6 -d 6 -L "hts_primers_output_red" -F
+   -P ${forward_primers} -Q ${reverse_primers} -l 5 -x -e 6 -d 6 -L "hts_primers_output_RED" -F
 done
