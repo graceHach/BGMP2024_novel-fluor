f6a2006753ff1ff9c1c7d6e5ac5eac841e4462d2
Grace Hach <ghach@login1.talapas.uoregon.edu>
2024-12-02 12:39:39 -0800

    Corrected

--- src/bins_to_MEF.sh
+++ src/bins_to_MEF.sh
@@ -3,7 +3,7 @@
 # Takes in file with bin edges and another with median bins for each variant and converts to 
 
 # Run blue
-./bins_to_MED.py --in_bin_edges "../bins/blue_bins.tsv"  --in_median_bins "../../../shared/dat/counts_per_bin/blue_median_bin.tsv" --out_file "../reports/blue_FINAL.tsv"
+./bins_to_MEF.py --in_bin_edges "../bins/blue_bins.tsv"  --in_median_bins "../../../shared/dat/counts_per_bin/blue_median_bin.tsv" --out_file "../reports/blue_FINAL.tsv"
 
 # Run red
-./bins_to_MED.py --in_bin_edges  "../bins/red_bins.tsv" --in_median_bins "../../../shared/dat/counts_per_bin/red_median_bin.tsv" --out_file "../reports/red_FINAL.tsv"
+./bins_to_MEF.py --in_bin_edges  "../bins/red_bins.tsv" --in_median_bins "../../../shared/dat/counts_per_bin/red_median_bin.tsv" --out_file "../reports/red_FINAL.tsv"
