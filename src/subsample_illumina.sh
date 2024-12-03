8470a9c472a70ab881f776f318e2de06923c30fe
Grace Hach <ghach@n0349.talapas.uoregon.edu>
2024-12-03 15:03:04 -0800

    Counting working

--- src/subsample_illumina.sh
+++ src/subsample_illumina.sh
@@ -13,9 +13,9 @@ mkdir -p $out_dir_blue
 for file in ${in_dir_blue}*.fastq.gz;
 do
     trimmed_filename=(${file##${in_dir_blue}})
-    #echo "$trimmed_filename"
-    #echo $trimmed_filename
-    zcat $file | head -n 10000 > ${out_dir_blue}$trimmed_filename
+    trimmed_filename=(${trimmed_filename%%.gz})
+    zcat $file | head -n 100 > ${out_dir_blue}$trimmed_filename
+    gzip ${out_dir_blue}$trimmed_filename 
 done
 
 mkdir -p $out_dir_red
@@ -23,7 +23,9 @@ mkdir -p $out_dir_red
 for file in ${in_dir_red}*.fastq.gz;
 do
     trimmed_filename=(${file##${in_dir_red}})
+    trimmed_filename=(${trimmed_filename%%.gz})
     #echo "$trimmed_filename"
     #echo $trimmed_filename
-    zcat $file | head -n 10000 > ${out_dir_red}$trimmed_filename
+    zcat $file | head -n 100 > ${out_dir_red}$trimmed_filename
+    gzip ${out_dir_red}$trimmed_filename 
 done
