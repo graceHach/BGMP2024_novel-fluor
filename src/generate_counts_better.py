6bab769abc5d3e739ca43e17fbc46b73b4095ed7
wesleygomersall <gomersallw@gmail.com>
2025-02-15 09:09:30 -0800

    Functional nextflow pipeline

--- src/generate_counts_better.py
+++ src/generate_counts_better.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python
-# 3 IS NECESSARY FOR EXECUTATION IN NEXTFLOW
 
 import argparse
 import gzip
-import glob
 import numpy as np
 
-# Run using helper script generate_counts.sh
-
-# input directory, output directory 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--in_dir",type=str,help="directory containing merged, primer trimmed reads", default="/projects/bgmp/shared/groups/2024/novel-fluor/shared/dat/blue_illum/")
-    #parser.add_argument("-f","--file_list", nargs='+')
-    parser.add_argument("-o","--out_file",type=str,help="", default="../reports/blue_counts_better_script.tsv")
+    parser.add_argument("-f","--file_list", nargs='+', type=str)
+    parser.add_argument("-o","--out_file", type=str, help="", default="../reports/blue_counts_better_script.tsv")
     return parser.parse_args()
 
 args = get_args()
+files = args.file_list # files == ['file2 file3 file1 file4']
+files = sorted(files[0].split()) # files == ['file1', 'file2', 'file3', 'file4']
 
 not_20_bp = 0
-# read in intermmediate file, initialize the dictionary
 print("initializing counts dict", flush=True)
 counts = {} 
-# keys are UMIs, values are numpy arrays with nine entries
+# keys are UMIs, values are numpy arrays with num_bins entries
 
-#files = glob.glob(args.in_dir+"*.fastq.gz")
-# glob puts these in a weird order 
-files = glob.glob(args.in_dir+"*.fastq.gz")
-# FILES LIST IS SPACE-SEPARATED LIST OF FILES
-files_list = sorted(list(args.in_dir))
-# sort the files???
-for file_index, file in enumerate(files_list):
+for file_index, file in enumerate(files):
     print("processing: ",file, flush=True)
     with gzip.open(file, 'rt') as fh:
         for index, line in enumerate(fh):
@@ -39,15 +28,13 @@ for file_index, file in enumerate(files_list):
                     if line[:-1] in counts:
                         counts[line[:-1]][file_index] += 1
                     else:
-                        # I USED INT8 INITIALLY AND IT FRICKING UNDERFLOWED AND HALF THE NUMBERS WERE NEGATIVE AAAAAUGH
-                        # 4.5 HOUR RUNTIME 
                         counts[line[:-1]] = np.zeros(9, dtype="int64")
                         counts[line[:-1]][file_index] = 1
                 else:
                     not_20_bp += 1
 
 # write list of file names as header of file
-header_line = "Barcode\t" + '\t'.join(files_list) + "\n"
+header_line = "Barcode\t" + '\t'.join(files) + "\n"
 
 print("Starting writing")
 keys = counts.keys()
